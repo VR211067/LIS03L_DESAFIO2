0201fe2d1a316a70b6be04e49af621091d24c3bc
MP220885 <lamparada04@gmail.com>
2025-04-29 15:24:32 -0600

    DetalleVenta.php

--- Models/DetalleVenta.php
+++ Models/DetalleVenta.php
@@ -2,6 +2,8 @@
 require_once 'Model.php';
 
 class DetalleVenta extends Model {
+
+    // Método para crear un nuevo registro en la tabla 'detalle_ventas'
     public function create($data) {
         return $this->set_query(
             "INSERT INTO detalle_ventas (venta_id, producto_id, cantidad, precio_unitario) VALUES (?, ?, ?, ?)",
